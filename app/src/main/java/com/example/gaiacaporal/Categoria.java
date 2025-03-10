215728c86fe9e868feaee4c7d7e7b326999713d5
Andrenickolands <nicosepulvedaamaya@gmail.com>
2025-03-10 14:46:45 -0500

    Nicolas Iconos_layouts

---
+++ app/src/main/java/com/example/gaiacaporal/Categoria.java
@@ -0,0 +1,13 @@
+package com.example.gaiacaporal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Categoria extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categoria);
+    }
+}
